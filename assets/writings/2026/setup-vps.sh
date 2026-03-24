c849c3a1a7b1b8433155d463459bb529973b7c09
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-03-24 11:05:01 -0700

    Updated script comments

--- assets/writings/2026/setup-vps.sh
+++ assets/writings/2026/setup-vps.sh
@@ -1,5 +1,5 @@
 # Assumes your VPS service has a way to view the VM's graphical display
-# Tested on Vultr
+# Tested on Vultr with Ubuntu 25.10
 
 # Part 1
 passwd && apt update && apt -y upgrade && apt install -y ubuntu-desktop && ubuntu-drivers install nvidia && ufw allow ssh && ufw enable && sed -i -e 's/auth	required	pam_succeed_if.so/#auth	required	pam_succeed_if.so/g' /etc/pam.d/gdm-password && reboot
