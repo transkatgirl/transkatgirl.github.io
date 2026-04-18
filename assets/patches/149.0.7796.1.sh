5b94629017580b37cc553eb84ebd864f7696ca9c
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-04-17 17:06:02 -0700

    Tweaked script

--- assets/patches/149.0.7796.1.sh
+++ assets/patches/149.0.7796.1.sh
@@ -1,2 +1,29 @@
+apt update && apt upgrade -y && apt install -y curl git lsb-release python3 git file vim sudo
+
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH="${HOME}/depot_tools:$PATH"
+echo 'export PATH="${HOME}/depot_tools:$PATH"' > ~/.bashrc
+
+mkdir ~/chromium && cd ~/chromium
+#fetch --nohooks chromium
+fetch --nohooks android
+cd src
+
+#echo "target_os = [ 'linux', 'android' ]" >> ../.gclient
+#gclient sync
+
 git checkout tags/149.0.7796.1
-git submodule update --recursive
+./build/install-build-deps.sh
+gclient revert
+gclient runhooks
+
+ensure_bootstrap
+gn args out/Default
+# target_os = "android"
+# target_cpu = "arm64"
+# use_remoteexec = false
+# is_component_build = false
+
+git apply "~/Android Chromium 149.0.7796.1.patch"
+
+autoninja -C out/Default chrome_public_apk
